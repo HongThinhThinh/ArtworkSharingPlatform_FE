899fadf6daf6704956a347bc61a866ea829ffcd1
NguyenHongThinh-2003 <ocbu0212@gmail.com>
2024-01-23 13:54:24 +0700

    fix homepage and router

--- src/config/router.jsx
+++ src/config/router.jsx
@@ -1,47 +1,26 @@
-import { useDispatch, useSelector } from "react-redux";
 import { Link, Outlet, createBrowserRouter } from "react-router-dom";
-import { increment } from "../redux/features/counterSlice";
-import { useEffect } from "react";
-import api from "./axios";
 import Header from "../component/header/Header";
 import Footer from "../component/footer/Footer";
 import HomePage from "../pages/homepage/HomePage";
 import Login from "../pages/login/Login";
 import SignUp from "../pages/signUp/SignUp";
 
-const PriveRoute = () => {
-  const count = useSelector((store) => store.counter);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    api.get(`/test2`).then((res) => {
-      console.log(res);
-      console.log(res);
-    });
-  }, [count]);
-  return (
-    <div>
-      <h1>
-        <Outlet />
-      </h1>
-      <Link to="/">Home</Link>
-      <button
-        onClick={() => {
-          dispatch(increment());
-        }}
-      >
-        +
-      </button>
-    </div>
-  );
-};
 export const router = createBrowserRouter([
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "register",
+    element: <SignUp />,
+  },
   {
     path: "/",
     element: (
       <div>
-        {/* <Header /> */}
+        <Header />
         <Outlet />
-        {/* <Footer /> */}
+        <Footer />
       </div>
     ),
     // outlet sẽ show thằng con của nó
@@ -49,14 +28,6 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <HomePage />,
-      },
-      {
-        path: "login",
-        element: <Login />,
-      },
-      {
-        path: "sign-up",
-        element: <SignUp />,
       },
     ],
   },
