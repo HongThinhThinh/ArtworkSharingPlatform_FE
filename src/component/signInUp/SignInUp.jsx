1be4f846e0a0150da56898126c2bff918cd2c305
NguyenNgocYNhi <89985195+NguyenNgocYNhi@users.noreply.github.com>
2024-01-30 12:48:14 +0700

    responsive-header

--- src/component/signInUp/SignInUp.jsx
+++ src/component/signInUp/SignInUp.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./SignInUp.scss";
 import { Avatar } from "antd";
+import { useMediaQuery } from "react-responsive";
 
 function SignInUp() {
+  const isMobile = useMediaQuery({ maxWidth: 562 });
   return (
     <div className="signInUp">
       <Link className="signInUp__login" to="/login">
         Log in
       </Link>
-
-      <Link className="signInUp__signup" to="/register">
-        Sign up
-      </Link>
+      {isMobile ? null : (
+        <Link className="signInUp__signup" to="/register">
+          Sign up
+        </Link>
+      )}
     </div>
   );
 }
