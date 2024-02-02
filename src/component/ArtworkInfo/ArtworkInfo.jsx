c2d3330f710cbfa87923a78c938bf66df3071870
NguyenHongThinh-2003 <ocbu0212@gmail.com>
2024-02-03 02:24:24 +0700

    fix ui page artworkDetails

--- src/component/ArtworkInfo/ArtworkInfo.jsx
+++ src/component/ArtworkInfo/ArtworkInfo.jsx
@@ -2,28 +2,28 @@ import React from "react";
 import "./ArtworkInfo.scss";
 import { GoHeart } from "react-icons/go";
 import { MdOutlineIosShare } from "react-icons/md";
+import ImgPreview from "../../pages/Image/Image";
+import { IoMdArrowBack } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 function ArtworkInfo() {
+  const navigate = useNavigate();
   return (
     <div className="artworkInfo">
-      <div className="artworkInfo--contentt">
-        <h2>The mushroom</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia</p>
+      <div className="artworkInfo--back">
+        <IoMdArrowBack onClick={() => navigate("/")} />
       </div>
       <div className="artworkInfo--img">
         <div className="artworkInfo--img__image">
-          <img
-            src="https://images.unsplash.com/photo-1706549717451-759b9a588511?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt=""
-          />
+          <ImgPreview src="https://images.unsplash.com/photo-1706549717451-759b9a588511?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         </div>
-        <div className="artworkInfo--img__icon">
+        {/* <div className="artworkInfo--img__icon">
           <div className="artworkInfo--img__icon__heart">
             <GoHeart />
           </div>
           <div className="artworkInfo--img__icon__share">
             <MdOutlineIosShare />
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- src/pages/ArtworkDetails/ArtworkDetails.jsx
+++ src/pages/ArtworkDetails/ArtworkDetails.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import RoomMessage from "../../component/roomMessage/RoomMessage";
 import "./ArtworkDetails.scss";
-import { FaRegCircle } from "react-icons/fa6";
 import ArtworkInfo from "../../component/ArtworkInfo/ArtworkInfo";
 import ListFeedback from "../../component/ListFeedback/ListFeedback";
 import CreatorWorkart from "../../sections/creatorWorkart/CreatorWorkart";
@@ -12,12 +11,6 @@ function ArtworkDetails() {
     <>
       <div className="artworkDetails">
         <div className="artworkDetails--left">
-          <RoomMessage
-            avt="https://scontent.fsgn5-9.fna.fbcdn.net/v/t39.30808-6/405274365_1295058761205189_4884784238252082746_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=SfSAh7w5UUIAX9VNPky&_nc_ht=scontent.fsgn5-9.fna&oh=00_AfAEoxvG68QbPMPmLHwG7NtiT4P8toTZmBnknMPgiRMvBg&oe=65C11307"
-            name="HongThinh"
-            lastMessage="Available for working"
-            icon={<FaRegCircle />}
-          />
           <div className="artworkDetails--left--content">
             <ArtworkInfo />
           </div>
