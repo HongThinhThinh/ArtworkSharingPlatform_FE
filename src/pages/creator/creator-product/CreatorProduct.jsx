124c1e0d1a6c3d22473c7d55e844f2be31958a70
NguyenHongThinh-2003 <ocbu0212@gmail.com>
2024-02-05 01:09:56 +0700

    add page profile creator

--- src/pages/creator/creator-product/CreatorProduct.jsx
+++ src/pages/creator/creator-product/CreatorProduct.jsx
@@ -4,6 +4,8 @@ import "./CreatorProduct.scss";
 import CreatorWorkart from "../../../sections/creatorWorkart/CreatorWorkart";
 import { useStateValue } from "../../../Context/StateProvider";
 import ImgPreview from "../../Image/Image";
+import { Button } from "antd";
+import CreatorProfile from "../creator-profile/CreatorProfile";
 
 function CreatorProduct() {
   const { theme } = useStateValue();
@@ -15,12 +17,7 @@ function CreatorProduct() {
         color: theme ? "#fff" : "#202020",
       }}
     >
-      <div className="creator-profilee">
-        <div className="creator-profilee__img">
-          <ImgPreview src="https://scontent.fsgn8-4.fna.fbcdn.net/v/t39.30808-6/401376601_1295831231127942_4331203260278541816_n.jpg?stp=cp6_dst-jpg&_nc_cat=105&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=khBll3vYxFIAX9Q_Sqm&_nc_ht=scontent.fsgn8-4.fna&oh=00_AfCLjg8Kp9kB-Iu1OKCxr2x-7mWiGQtL9nHf8OgoPEkhGA&oe=65C3D350" />
-        </div>
-      </div>
-
+      <CreatorProfile />
       <div className="listArtWork">
         <DropdownTop />
         <div className="listArtWork--items">
--- src/pages/creator/creator-profile/CreatorProfile.jsx
+++ src/pages/creator/creator-profile/CreatorProfile.jsx
@@ -1,7 +1,40 @@
 import React from "react";
-
+import ImgPreview from "../../Image/Image";
+import { Button } from "antd";
+import "./CreatorProfile.scss";
+import { IoMdNotificationsOutline } from "react-icons/io";
 function CreatorProfile() {
-  return <div>Creator</div>;
+  return (
+    <div className="wrap-creatorProfile">
+      <div className="header-noti">
+        <IoMdNotificationsOutline />
+      </div>
+      <div className="creator-profilee">
+        <div className="creator-profilee__img">
+          <ImgPreview src="https://i.pinimg.com/236x/5b/5f/f7/5b5ff7b9fc4be5cc8abf3b4b53033b25.jpg" />
+        </div>
+        <div className="creator-profilee__info">
+          <div className="creator-profilee__info__name">
+            <h2>Hong Thinh</h2>
+            <Button
+              className="login__form__container__namepass__submit"
+              htmlType="submit"
+              // style={{backgroundColor: theme?"#1677ff":""}}
+            >
+              Edit Profile
+            </Button>
+          </div>
+          <div className="creator-profilee__info__follower">
+            <h4>20 Post </h4>
+            <span>|</span>
+            <h4>200 follower</h4>
+            <span>|</span>
+            <h4>200 follower</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreatorProfile;
