e6b6ab50538bfb1f32581719c46d0eb1bcb3d423
NguyenHongThinh-2003 <ocbu0212@gmail.com>
2024-02-05 23:16:43 +0700

    add test sign for creator

--- src/test.jsx
+++ src/test.jsx
@@ -1,42 +1,63 @@
-import React, { useEffect, useState } from "react";
-import uploadFile from "./assets/hook/useUpload";
-import UploadImg from "./assets/UploadImg/UploadImg";
-import { Link } from "react-router-dom";
-import api from "./config/axios";
+import React, { useState, useRef } from "react";
+import Popup from "reactjs-popup";
+import SignaturePad from "react-signature-canvas";
+import "./index.css";
+function App() {
+  const [imageURL, setImageURL] = useState(null); // create a state that will contain our image url
 
-function Test() {
-  const [file, setFile] = useState({});
-  const [URL, setURL] = useState("");
-  const getLink = async () => {
-    console.log(file);
-    let URL = await uploadFile(file);
-    setURL(URL);
-  };
+  const sigCanvas = useRef({});
 
-  useEffect(() => {
-    getLink();
-    // getAll();
-  }, [file]);
+  /* a function that uses the canvas ref to clear the canvas 
+  via a method given by react-signature-canvas */
+  const clear = () => sigCanvas.current.clear();
 
-  console.log(URL);
+  /* a function that uses the canvas ref to trim the canvas 
+  from white spaces via a method given by react-signature-canvas
+  then saves it in our state */
+  const save = () =>
+    setImageURL(sigCanvas.current.getTrimmedCanvas().toDataURL("image/png"));
+
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        height: "100vh",
-        gap: "30px",
-      }}
-    >
-      <h1>Test Upload IMG</h1>
-      <div onChange={(e) => setFile(e.target.files[0])}>
-        <UploadImg />
-        <div>{URL && <a href={URL}> link ảnh của bạn đây </a>}</div>
-      </div>
+    <div className="App">
+      <h1>Signature Pad Example</h1>
+      <Popup
+        modal
+        trigger={<button>Open Signature Pad</button>}
+        closeOnDocumentClick={false}
+      >
+        {(close) => (
+          <>
+            <SignaturePad
+              ref={sigCanvas}
+              canvasProps={{
+                className: "signatureCanvas",
+              }}
+            />
+            {/* Button to trigger save canvas image */}
+            <button onClick={save}>Save</button>
+            <button onClick={clear}>Clear</button>
+            <button onClick={close}>Close</button>
+          </>
+        )}
+      </Popup>
+      <br />
+      <br />
+      {/* if our we have a non-null image url we should 
+      show an image and pass our imageURL state to it*/}
+      {imageURL ? (
+        <img
+          src={imageURL}
+          alt="my signature"
+          style={{
+            display: "block",
+            margin: "0 auto",
+            border: "1px solid black",
+            width: "150px",
+          }}
+        />
+      ) : null}
     </div>
   );
 }
 
-export default Test;
+export default App;
