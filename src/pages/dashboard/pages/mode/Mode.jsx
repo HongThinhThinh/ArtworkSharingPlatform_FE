34be0b4ae58e1e2828c5ebf532e719e9a1a3efef
NguyenNgocYNhi <89985195+NguyenNgocYNhi@users.noreply.github.com>
2024-02-09 17:27:41 +0700

    dashboard

--- src/pages/dashboard/pages/mode/Mode.jsx
+++ src/pages/dashboard/pages/mode/Mode.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import UserTable from "../../../../component/table/TableDashboard";
 
 function Mode() {
-  return <div>Mode</div>;
+  return (
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+      className="mode"
+    >
+      <UserTable />
+    </div>
+  );
 }
 
 export default Mode;
