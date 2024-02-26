ad2f758e400963a78aca9e203397b4b42b4031d8
NguyenNgocYNhi <89985195+NguyenNgocYNhi@users.noreply.github.com>
2024-02-27 04:06:41 +0700

    creator-offer-order

---
+++ src/component/uploadDemo/UploadDemo.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
+import { message, Upload } from "antd";
+import "./UploadDemo.scss";
+const getBase64 = (img, callback) => {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => callback(reader.result));
+  reader.readAsDataURL(img);
+};
+const beforeUpload = (file) => {
+  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+  if (!isJpgOrPng) {
+    message.error("You can only upload JPG/PNG file!");
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error("Image must smaller than 2MB!");
+  }
+  return isJpgOrPng && isLt2M;
+};
+const UploadDemo = () => {
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState();
+  const handleChange = (info) => {
+    if (info.file.status === "uploading") {
+      setLoading(true);
+      return;
+    }
+    if (info.file.status === "done") {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj, (url) => {
+        setLoading(false);
+        setImageUrl(url);
+      });
+    }
+  };
+  const uploadButton = (
+    <button
+      style={{
+        border: 0,
+        background: "none",
+      }}
+      type="button"
+    >
+      {loading ? (
+        <LoadingOutlined style={{ color: "#B42D81", fontSize: "3em" }} />
+      ) : (
+        <PlusOutlined style={{ color: "#B42D81", fontSize: "3em" }} />
+      )}
+    </button>
+  );
+  return (
+    <>
+      <Upload
+        name="demo"
+        listType="picture-card"
+        className="demo-uploader"
+        showUploadList={false}
+        action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
+        beforeUpload={beforeUpload}
+        onChange={handleChange}
+      >
+        {imageUrl ? (
+          <img
+            src={imageUrl}
+            alt="demo"
+            style={{
+              width: "100%",
+            }}
+          />
+        ) : (
+          uploadButton
+        )}
+      </Upload>
+    </>
+  );
+};
+export default UploadDemo;
--- src/pages/requestOrder/RequestOrder.jsx
+++ src/pages/requestOrder/RequestOrder.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./RequestOrder.scss";
 import RequestOrderDetail from "../../component/RequestOrderDetail/RequestOrderDetail";
 import RequestOrderList from "../../component/requestOrderList/RequestOrderList";
+import InProgressOrderDetail from "../../component/requestOrderDetail/InProgressOrderDetail";
 
 function RequestOrder() {
   const [choice, setChoice] = useState(-1);
@@ -9,6 +10,7 @@ function RequestOrder() {
     <div className="requestOrder">
       <RequestOrderList choice={choice} setChoice={setChoice} />
       <RequestOrderDetail choice={choice} setChoice={setChoice} />
+      {/* <InProgressOrderDetail choice={choice} setChoice={setChoice} /> */}
     </div>
   );
 }
