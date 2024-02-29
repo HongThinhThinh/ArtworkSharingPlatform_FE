8a4a54c260710847c8347166884d747f508ea176
NguyenNgocYNhi <89985195+NguyenNgocYNhi@users.noreply.github.com>
2024-03-01 01:24:13 +0700

    change-expect-form

--- src/component/requestOrderDetail/RequestOrderDetail.jsx
+++ src/component/requestOrderDetail/RequestOrderDetail.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "./RequestOrderDetail.scss";
-import { Avatar, Button, Form, InputNumber, Modal } from "antd";
+import { Avatar, Button, Form, Input, InputNumber, Modal, Select } from "antd";
 import { AiFillMessage } from "react-icons/ai";
 import { useMediaQuery } from "react-responsive";
 import {
@@ -38,6 +38,7 @@ function RequestOrderDetail({ choice, setChoice, data, setData }) {
   }, [data]);
 
   const onFinish = async (fieldsValue) => {
+    console.log(fieldsValue);
     const rangeValue = fieldsValue["range-picker"];
     const rangeTimeValue = fieldsValue["range-time-picker"];
     const values = {
@@ -60,6 +61,16 @@ function RequestOrderDetail({ choice, setChoice, data, setData }) {
     }
     setModal2Open(false);
   };
+  const selectAfter = (
+    <Form.Item name="unit" noStyle initialValue="hours">
+      <Select initialValue="hours">
+        <Option value="hours">hours</Option>
+        <Option value="days">days</Option>
+        <Option value="months">months</Option>
+        <Option value="years">years</Option>
+      </Select>
+    </Form.Item>
+  );
 
   return (
     <div className={`request-order-detail ${choice != -1 ? "active" : ""}`}>
@@ -182,13 +193,10 @@ function RequestOrderDetail({ choice, setChoice, data, setData }) {
                     >
                       <InputNumber />
                     </Form.Item>
-                    <span className="ant-form-text" style={{ marginLeft: 8 }}>
-                      $
-                    </span>
                   </Form.Item>
 
-                  <Form.Item name="date" label="Deadline" {...config}>
-                    <DatePicker showTime format="YYYY-MM-DD HH:mm:ss" />
+                  <Form.Item name="date" label="Deadline">
+                    <InputNumber addonAfter={selectAfter} />
                   </Form.Item>
                 </div>
                 <Form.Item>
