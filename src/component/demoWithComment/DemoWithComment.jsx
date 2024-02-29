4603197c262942c6119607de29292e16551fd300
NguyenNgocYNhi <89985195+NguyenNgocYNhi@users.noreply.github.com>
2024-03-01 01:07:14 +0700

    DemoWithComment

--- src/component/demoWithComment/DemoWithComment.jsx
+++ src/component/demoWithComment/DemoWithComment.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { CommentOutlined } from "@ant-design/icons";
+import { Modal } from "antd";
+import React, { useState } from "react";
+import ImgPreview from "../../pages/Image/Image";
+import "./DemoWithComment.scss";
+import TextArea from "antd/es/input/TextArea";
 
-function DemoWithComment() {
-  return <div>DemoWithComment</div>;
+function DemoWithComment({ imageUrl }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <div className="demo">
+      <ImgPreview
+        width={200}
+        height={200}
+        style={{
+          borderRadius: "10px",
+          border: "none",
+          outline: "none",
+        }}
+        src={imageUrl}
+      />
+      <CommentOutlined className="demo__icon" onClick={showModal} />
+      <Modal
+        title="Comment of Demo"
+        open={isModalOpen}
+        footer={null}
+        onCancel={closeModal}
+      >
+        {/* <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p> */}
+        <TextArea />
+      </Modal>
+    </div>
+  );
 }
 
 export default DemoWithComment;
