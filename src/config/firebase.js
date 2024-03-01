0b146fead3bc6b9407a874596975d21466eea7be
NguyenHongThinh-2003 <ocbu0212@gmail.com>
2024-03-01 18:01:17 +0700

    add config message firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,3 +23,29 @@ export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
 export const db = getFirestore();
+export const messaging = getMessaging();
+
+async function requestPermission() {
+  console.log("Requesting permission...");
+  await Notification.requestPermission();
+  return getKey();
+}
+
+const getKey = async () => {
+  const token = await getToken(messaging, {
+    vapidKey:
+      "BOwbfM6evRixiJL4zQjwJifRjQAWYSApFfwAWRVR1zBwIOzGbblQNqJ495LcPlb-sPtNrn4QD3qQF3cCbuNhDuQ",
+  });
+
+  return token;
+};
+
+const listen = () => {
+  console.log("listening");
+  onMessage(messaging, (payload) => {
+    console.log("Message received. ", payload);
+    // ...
+  });
+};
+
+export { requestPermission, listen };
