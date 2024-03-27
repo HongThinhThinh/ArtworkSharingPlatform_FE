b576818c2bb43232cd0e15467732cc02a9a6fe64
NguyenHongThinh-2003 <ocbu0212@gmail.com>
2024-03-28 02:04:30 +0700

    add

--- src/pages/dashboard/pages/AI/AIPage.jsx
+++ src/pages/dashboard/pages/AI/AIPage.jsx
@@ -11,7 +11,7 @@ function AIPage() {
   const [prompt, setPrompt] = useState("A cute baby sea otter");
   const [generatedImages, setGeneratedImages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const aaa = "ABC";
+  const OPENAI_API_KEY = "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";
   async function generateImages() {
     setRobot(drawing);
     setIsClick(true);
@@ -28,7 +28,7 @@ function AIPage() {
       };
       const headers = {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${aaa}`,
+        Authorization: `Bearer ${OPENAI_API_KEY}`,
       };
 
       const response = await axios.post(
