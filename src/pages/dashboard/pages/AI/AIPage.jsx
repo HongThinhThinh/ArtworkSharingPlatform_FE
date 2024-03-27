de04398d283416bfb910ff1c7d63d48978018400
NguyenHongThinh-2003 <ocbu0212@gmail.com>
2024-03-28 01:50:26 +0700

    fix api key

--- src/pages/dashboard/pages/AI/AIPage.jsx
+++ src/pages/dashboard/pages/AI/AIPage.jsx
@@ -11,7 +11,7 @@ function AIPage() {
   const [prompt, setPrompt] = useState("A cute baby sea otter");
   const [generatedImages, setGeneratedImages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const OPENAI_API_KEY = "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";
+  const OPENAI_API_KEY = "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";
   async function generateImages() {
     setRobot(drawing);
     setIsClick(true);
@@ -21,7 +21,7 @@ function AIPage() {
       setGeneratedImages(drawing);
       const requestData = {
         model: "dall-e-3",
-        prompt: "A cute baby sea otter",
+        prompt: prompt,
         n: 1,
         size: "1024x1024",
         quality: "hd",
