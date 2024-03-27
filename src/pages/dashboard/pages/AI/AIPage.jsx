49c283a797e9f8f3faaa92e21d46a2eb265c1894
NguyenHongThinh-2003 <ocbu0212@gmail.com>
2024-03-28 02:03:04 +0700

    add api ai

--- src/pages/dashboard/pages/AI/AIPage.jsx
+++ src/pages/dashboard/pages/AI/AIPage.jsx
@@ -11,7 +11,7 @@ function AIPage() {
   const [prompt, setPrompt] = useState("A cute baby sea otter");
   const [generatedImages, setGeneratedImages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  // const OPENAI_API_KEY = "";
+  const OPENAI_API_KEY = "ABC";
   async function generateImages() {
     setRobot(drawing);
     setIsClick(true);
