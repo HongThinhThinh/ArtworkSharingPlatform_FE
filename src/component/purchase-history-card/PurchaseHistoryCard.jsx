f1a05193c3bd63b927e38b023dc5a6e8e34dc0fb
baotran1311 <baotran13112003@gmail.com>
2024-03-28 10:39:32 +0700

    api history purchase

--- src/component/purchase-history-card/PurchaseHistoryCard.jsx
+++ src/component/purchase-history-card/PurchaseHistoryCard.jsx
@@ -4,11 +4,23 @@ import { Button } from "antd";
 import "./PurchaseHistoryCard.scss";
 
 function PurchaseHistoryCard({ src, date, description, title }) {
+  const convert = () => {
+    var transactionDate = date;
+    var year = transactionDate.substring(0, 4);
+    var month = transactionDate.substring(4, 6) - 1; // Month in JavaScript Date object is 0-indexed
+    var day = transactionDate.substring(6, 8);
+    var hour = transactionDate.substring(8, 10);
+    var minute = transactionDate.substring(10, 12);
+    var second = transactionDate.substring(12, 14);
+
+    var newDate = new Date(year, month, day, hour, minute, second).toDateString();
+    return newDate;
+  };
   return (
     <div className="purchase-hi-card">
       <div className="purchase-hi-card__left">
         <h1>Title: {title}</h1>
-        <h3>Purchased date: {date}</h3>
+        <h3>Purchased date: {convert()}</h3>
         <p>
           <strong>Description:</strong> {description}.
         </p>
