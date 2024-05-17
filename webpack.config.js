ee5604972319f10ebb898afa730e716186f1e2a9
NguyenHongThinh-2003 <ocbu0212@gmail.com>
2024-05-17 13:49:05 +0700

    config webpackage

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+  devServer: {
+    https: {
+      key: fs.readFileSync("/path/to/server.key"),
+      cert: fs.readFileSync("/path/to/server.crt"),
+      ca: fs.readFileSync("/path/to/ca.pem"),
+    },
+  },
+};
